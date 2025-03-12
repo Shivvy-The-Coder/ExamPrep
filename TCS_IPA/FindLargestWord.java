a487190c5f367551ed9a3b1c4e7185e8fe25b1d4
Shivam <139750193+Shivvy-The-Coder@users.noreply.github.com>
2025-03-13 03:05:06 +0530

    largest word in entire senetnce has been added

--- TCS_IPA/FindLargestWord.java
+++ TCS_IPA/FindLargestWord.java
@@ -12,16 +12,14 @@ public class FindLargestWord {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        String[] arr = str.split(" ");   // splitting the whole sentence based on whitespaces between words
-        String largest = "";
-        for(int i = 0; i < arr.length; i++)
-        {
-            if(arr[i].length() > largest.length())
-            {
-                largest = arr[i];
-            }
-        }
-        System.out.println(largest);
+        String arr [] = str.split(" ");
+        int len =Integer.MIN_VALUE;
 
+        for (int i=0;i<arr.length;i++)
+        {
+            if(arr[i].length()>len)
+                len=arr[i].length();
+        }
+        System.out.println(len);
     }
 }
